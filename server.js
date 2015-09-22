f5d9ae9cd95939300d3aef7a612a40e00bbb6fd5
Admin <admin@Admins-MacBook-Pro.local>
2015-09-22 15:40:21 +0530

    Day02

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ var http = require("http");
 var fs = require("fs");
 var server = http.createServer(function(req,res){
 	var url = req.url;
-	var filePath = url.substring(1);
-	loadFile(filePath,res);
+	if(url.match(/info/)){
+		getCountryInfo(req,res);	
+	}
+	else{
+		var filePath = url.substring(1);
+		loadFile(filePath,res);
+	}
 });
+function getCountryInfo(req,res){
+	var queryString = require("url").parse(req.url,true).query;
+	var country = queryString.country;
+	var capital = "NA";
+	if(country == "India")
+		capital = "New Delhi";
+	else if(country == "USA")
+		capital = "Washington DC";
+	res.end(capital);
+}
 function loadFile(filePath,res){
 	fs.readFile(filePath,function(err,data){
 		if(err)
